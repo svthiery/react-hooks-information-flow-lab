019eee5782a769fa0f26f993f6e61a674e9a6d6d
Sarah Thiery <sarahvthiery@gmail.com>
2021-02-02 17:58:27 -0500

    commit tree 9a39a28508c6159c9744a3a42ff9a8090837445e

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+function Header(props) {
+    return (
+        <header>
+        <h2>Shopster</h2>
+        <button onClick={props.onDarkModeClick}>
+          {props.isDarkMode ? "Dark" : "Light"} Mode
+        </button>
+      </header>
+    );
+};
+
+export default Header;
